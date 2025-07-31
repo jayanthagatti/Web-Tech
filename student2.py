dc0546ee4d00fe0dee89b2cc56ddb175b4b4649e
jayanthagatti <jayanthagatti2003@gmail.com>
2025-07-31 10:10:58 +0530

    my first code

---
+++ student2.py
@@ -0,0 +1,12 @@
+class Student:
+    def __init__(self):
+        self.name="jayanth"
+        self.age=22
+        self.usn=40
+    def study(self):
+        print("jayanth is not studying")
+s1=Student()
+print(s1.name)
+print(s1.age)
+print(s1.usn)
+s1.study()
